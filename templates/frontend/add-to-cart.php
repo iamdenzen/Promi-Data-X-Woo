55cb30bdb65746f3eaec4d4cfff5cf0fc171e613
iamdenzen <39949175+iamdenzen@users.noreply.github.com>
2026-08-17 18:59:44 +0500

    added woo support to template

--- templates/frontend/add-to-cart.php
+++ templates/frontend/add-to-cart.php
@@ -774,6 +774,8 @@ $is_price_on_request =
 								<?php echo esc_html__( 'Anfrage senden', 'promi-data-x-woo' ); ?>
 							</button>
 
+							<?php do_action('woocommerce_after_add_to_cart_button'); ?>
+
 						</div>
 
 					</div>
